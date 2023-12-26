289c07f870fc7d111d20aadba29089c86afd9baf
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-12-26 07:25:51 +0200

    feat: Add test for NextToken function in lexer_test.go

--- src/token/token.go
+++ src/token/token.go
@@ -12,23 +12,48 @@ const (
 	EOF     = "EOF"
 
 	// Identifiers + literals
-	IDENT 	= "IDENT" // add, foobar, x, y, ...
-	INT 	= "INT" // 1343456
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
 
 	// Operators
-	ASSIGN 	= "="
-	PLUS 	= "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
 
 	// Delimiters
-	COMMA 	= ","
+	COMMA     = ","
 	SEMICOLON = ";"
 
-	LPAREN 	= "("
-	RPAREN 	= ")"
-	LBRACE 	= "{"
-	RBRACE 	= "}"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
 	// Keywords
 	FUNCTION = "FUNCTION"
-	LET 	= "LET"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+	"fn"	: FUNCTION,
+	"let"	: LET,
+	"true"	: TRUE,
+	"false"	: FALSE,
+	"if"	: IF,
+	"else"	: ELSE,
+	"return": RETURN,
+}
